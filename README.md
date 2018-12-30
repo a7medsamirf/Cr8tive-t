7f2c71f87723320e262f7509903ec349fdc20851
ahmed samir <ahmed.samir.farid@gmail.com>
2018-12-30 23:09:12 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Cr8tive
+# Cr8tive Template Version: 1.0
+## Depends on  Bootstrap Library
+This Template For Freelancers To Define Themselves And Display Their Works.
+
+Review : [Cr8tive Demo](https://a7medsamirf.github.io/Cr8tive/)
+
+## Content:
+1. CSS Foder
+     - Font Awesome > all.min.css
+     - Animate      > animate.css
+     - Bootstrap    > bootstrap.css
+     - fancybox     > jquery.fancybox.css 
+     - slick        > slick-theme.css
+     - slick        > slick.css
+     - Style        > style.css
+    
+2. Template Images > All Images and illustrations are just for demonstration, all right reserved by Vijay Verma.
+
+3. Js Foder
+     - Bootstrap  > bootstrap.min.js	 bootstrap.min. V 4.2.1
+     - jquery     > jquery-3.3.1.min.js	
+     - custom     > custom.js	  
+     - popper     > popper.min.js
+     - wow        > wow.min.js
+     - smooth     > smooth-scroll.js
+     - fancybox   > jquery.fancybox.min.js
+     - slick      > slick.min.js
+
+3. Web Fonts 
+
+4. License
+
+5. index.html
+
+6. logo.ico    
+    
+ ## Languages and libraries  
+ - Html5
+ - Css3
+ - Bootstrap 4
+ - jquery 3.3.1
+ - Wow js
+ - fancybox js
+ - slick js
+ 
+
+### License
+
+[MIT](https://github.com/ahmedfaheem/Glazzy-Theme/blob/master/License).
+
+### Support or Contact
+ Copyright (c) 2018 Ahmed samir  ahmed.samir.farid@gmail.com github.com/a7medsamirf
+ 
+ ### [Facebook Account](https://www.facebook.com/a7med.samir.f)
+
---
+++ assets/js/custom.js
@@ -0,0 +1,118 @@
+/*global $, alert, console*/
+
+// Changing nav-bar color after scrolling
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+  
+  // Start WOW
+  $(document).ready(function () {
+    new WOW().init();
+    }); 
+
+  // Start niceScroll
+  $(document).ready(function () {
+    $("html").niceScroll({
+      scrollspeed: "100",
+      cursorcolor: "#eeb501",
+      cursorborder: "3px solid #eeb501",
+      cursorborderradius:0,
+      sensitiverail: false,
+  });
+}); 
+     
+    // Start dropdown
+  $('.dropdown').on('show.bs.dropdown', function (e) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+  
+  $('.dropdown').on('hide.bs.dropdown', function (e) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+  
+
+  // Start SmoothScroll
+  $(document).ready(function () {
+    var scroll = new SmoothScroll('a[href*="#"]');
+    }); 
+
+
+$(document).ready(function () {
+    $('#testimonial-slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+      });
+    
+
+
+      $(document).ready(function () {
+        $('#clients-slider').slick({
+            dots: false,
+            arrows: false,
+            autoplay: true,
+            infinite: true,
+            speed: 600,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          });
+          });
+    
